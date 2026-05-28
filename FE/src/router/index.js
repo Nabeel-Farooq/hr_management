a9d2d3b4c7c82396129adc2b29f75b493966ac47
Nabeel Farooq <45237521+Nabeel-Farooq@users.noreply.github.com>
2026-05-28 14:04:45 +0500

    Update index.js

--- FE/src/router/index.js
+++ FE/src/router/index.js
@@ -1,40 +1,58 @@
 import { route } from 'quasar/wrappers'
-import { createRouter, createMemoryHistory, createWebHistory, createWebHashHistory } from 'vue-router'
+import {
+  createMemoryHistory,
+  createRouter,
+  createWebHashHistory,
+  createWebHistory,
+  type RouteLocationNormalized
+} from 'vue-router'
+
 import routes from './routes'
 
-/*
- * If not building with SSR mode, you can
- * directly export the Router instantiation;
- *
- * The function below can be async too; either use
- * async/await or return a Promise which resolves
- * with the Router instance.
- */
+const isServer = process.env.SERVER
+const isHistoryMode = process.env.VUE_ROUTER_MODE === 'history'
+const routerBase = process.env.MODE === 'ssr'
+  ? undefined
+  : process.env.VUE_ROUTER_BASE
 
-export default route(function ( { store, ssrContext } ) {
-  const createHistory = process.env.SERVER
-    ? createMemoryHistory
-    : (process.env.VUE_ROUTER_MODE === 'history' ? createWebHistory : createWebHashHistory)
+const historyFactory = isServer
+  ? createMemoryHistory
+  : isHistoryMode
+    ? createWebHistory
+    : createWebHashHistory
 
-  const Router = createRouter({
-    scrollBehavior: () => ({ left: 0, top: 0 }),
+function requiresAuth(to: RouteLocationNormalized): boolean {
+  return to.matched.some(route => route.meta.requireLogin)
+}
+
+export default route(({ store }) => {
+  const router = createRouter({
+    history: historyFactory(routerBase),
+
     routes,
 
-    // Leave this as is and make changes in quasar.conf.js instead!
-    // quasar.conf.js -> build -> vueRouterMode
-    // quasar.conf.js -> build -> publicPath
-    history: createHistory(process.env.MODE === 'ssr' ? void 0 : process.env.VUE_ROUTER_BASE)
+    scrollBehavior() {
+      return {
+        left: 0,
+        top: 0
+      }
+    }
   })
 
-  Router.beforeEach((to, from, next) => {
-    const auth = store.state.auth
-    if (to.matched.some(record => record.meta.requireLogin) && !auth.isAuthenticated) {
-      next({
+  router.beforeEach((to) => {
+    const isAuthenticated = store.state.auth?.isAuthenticated
+
+    if (requiresAuth(to) && !isAuthenticated) {
+      return {
         name: 'Login',
-        query: { to: to.path }
-      })
-    } else next()
+        query: {
+          to: to.fullPath
+        }
+      }
+    }
+
+    return true
   })
 
-  return Router
+  return router
 })
