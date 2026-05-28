f0bfa86eaf43da291faef04e74e65a5b453fa211
Nabeel Farooq <45237521+Nabeel-Farooq@users.noreply.github.com>
2026-05-28 14:05:04 +0500

    Update routes.js

--- FE/src/router/routes.js
+++ FE/src/router/routes.js
@@ -1,83 +1,101 @@
-const routes = [
+import type { RouteRecordRaw } from 'vue-router'
+
+const authChildren: RouteRecordRaw[] = [
   {
-    name: "Login",
-    path: "/login",
-    component: () => import("pages/Login.vue"),
+    name: 'List Employee KT',
+    path: 'list-employee-kt',
+    component: () => import('src/pages/Employee/ListEmployeeForKT.vue')
   },
   {
-    path: "/",
-    component: () => import("layouts/MainLayout.vue"),
-    meta: { requireLogin: true },
+    name: 'List Employee QTNS',
+    path: 'list-employee-qtns',
+    component: () => import('src/pages/Employee/ListEmployeeForQTNS.vue')
+  },
+  {
+    name: 'List Employee',
+    path: 'list-employee',
+    component: () => import('src/pages/Employee/ListEmployee.vue')
+  },
+  {
+    name: 'Update Personal Information',
+    path: 'self-update',
+    component: () => import('pages/Account/UpdateAccountInformation.vue')
+  },
+  {
+    name: 'Change Password',
+    path: 'change-password',
+    component: () => import('pages/Account/ChangePassword.vue')
+  },
+  {
+    name: 'Category',
+    path: 'category',
+    component: () => import('pages/Category/Category.vue')
+  },
+  {
+    name: 'Position',
+    path: 'position',
+    component: () => import('pages/Position/Position.vue')
+  },
+  {
+    name: 'Department',
+    path: 'department',
+    component: () => import('pages/Department/Department.vue')
+  },
+  {
+    name: 'List Project',
+    path: 'project',
+    component: () => import('pages/Project/ListProject.vue')
+  },
+  {
+    name: 'List View Project',
+    path: 'project-view',
+    component: () => import('pages/Project/ListViewProject.vue')
+  },
+  {
+    name: 'List Account',
+    path: 'list-account-admin',
+    component: () => import('pages/Account/ListAccount.vue')
+  },
+  {
+    name: 'List Account QTDA',
+    path: 'list-account-qtda',
+    component: () => import('pages/Account/ListAccountQTDA.vue')
+  }
+]
+
+const routes: RouteRecordRaw[] = [
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import('pages/Login.vue'),
+    meta: {
+      guestOnly: true
+    }
+  },
+
+  {
+    path: '/',
+    component: () => import('layouts/MainLayout.vue'),
+
+    meta: {
+      requireLogin: true
+    },
+
     children: [
       {
-        name: "List Employee KT",
-        path: "/list-employee-kt",
-        component: () => import("src/pages/Employee/ListEmployeeForKT.vue"),
+        path: '',
+        redirect: '/list-employee'
       },
-      {
-        name: "List Employee QTNS",
-        path: "/list-employee-qtns",
-        component: () => import("src/pages/Employee/ListEmployeeForQTNS.vue"),
-      },
-      {
-        name: "List Employee",
-        path: "/list-employee",
-        component: () => import("src/pages/Employee/ListEmployee.vue"),
-      },
-      {
-        name: "Update Personal Information",
-        path: "/self-update",
-        component: () => import("pages/Account/UpdateAccountInformation.vue"),
-      },
-      {
-        name: "Change Password",
-        path: "/change-password",
-        component: () => import("pages/Account/ChangePassword.vue"),
-      },
-      {
-        name: "Category",
-        path: "/category",
-        component: () => import("pages/Category/Category.vue"),
-      },
-      {
-        name: "Position",
-        path: "/position",
-        component: () => import("pages/Position/Position.vue"),
-      },
-      {
-        name: "Department",
-        path: "/department",
-        component: () => import("pages/Department/Department.vue"),
-      },
-      {
-        name: "List Project",
-        path: "/project",
-        component: () => import("pages/Project/ListProject.vue"),
-      },
-      {
-        name: "List View Project",
-        path: "/project-view",
-        component: () => import("pages/Project/ListViewProject.vue"),
-      },
-      {
-        name: "List Account",
-        path: "/list-account-admin",
-        component: () => import("pages/Account/ListAccount.vue"),
-      },
-      {
-        name: "List Account QTDA",
-        path: "/list-account-qtda",
-        component: () => import("pages/Account/ListAccountQTDA.vue"),
-      },
-    ],
+
+      ...authChildren
+    ]
   },
 
-  // Always leave this as last one,
-  // but you can also remove it
   {
-    path: "/:catchAll(.*)*",
-    component: () => import("pages/Error404.vue"),
-  },
-];
+    path: '/:catchAll(.*)*',
+    name: 'Not Found',
+    component: () => import('pages/Error404.vue')
+  }
+]
 
-export default routes;
+export default routes
